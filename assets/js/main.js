60c0c3a82be9f4168cf903591a5e140ef6bf8f00
Leen-A-Saleh <https://github.com/Leen-A-Saleh>
2024-05-04 22:09:21 +0300

    image with grid view

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// onclick
+function full(){
+document.getElementById('column1').style.width='100%';
+document.getElementById('column2').style.width='100%';
+document.getElementById('column3').style.width='100%';
+document.getElementById('column4').style.width='100%';
+}
+function twoCol(){
+  document.getElementById('column1').style.width='50%';
+  document.getElementById('column2').style.width='50%';
+  document.getElementById('column3').style.width='50%';
+  document.getElementById('column4').style.width='50%';
+}
+function fourCol(){
+  document.getElementById('column1').style.width='25%';
+  document.getElementById('column2').style.width='25%';
+  document.getElementById('column3').style.width='25%';
+  document.getElementById('column4').style.width='25%';
+}
